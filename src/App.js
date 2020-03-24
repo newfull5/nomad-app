94e005599c32f61a23096b65afa61018cf8f9e03
newfull5 <dhtocks@naver.com>
2020-03-24 17:09:45 +0900

    #4.2 Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import PropTypes from "prop-types";
-
+import axios from 'axios';
+import Movie from "./Movie";
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
 
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
-    
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
   }
 
-  render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  render() {
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." : movies.map(movie => {
+      console.log(movie);
+      return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster} />
+    })}</div>
   }
 }
 
