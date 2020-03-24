465377ae4bc0d15ef7564e9f5190331daf1a0bc1
newfull5 <dhtocks@naver.com>
2020-03-24 16:22:12 +0900

    4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import Potato from "./Potato";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      <Potato />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 6000);
+    
+  }
+
+  render(){
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
 }
 
 export default App;
